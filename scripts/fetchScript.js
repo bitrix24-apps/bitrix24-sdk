8c38d70b2be504ea35be2db4b9364d5cedeaa670
Alexandr Tovmach <alexandrtovmach@gmail.com>
2022-01-05 02:34:22 +0200

    feat: Added script fetcher

---
+++ scripts/fetchScript.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const fs = require("fs");
+const https = require("https");
+
+const target = "../src/bitrix24-api.js";
+
+const fetchOriginalScript = async () => {
+  const file = fs.createWriteStream(path.resolve(__dirname, target));
+  https.get("https://api.bitrix24.com/api/v1/", (response) => {
+    response.pipe(file);
+  });
+};
+
+fetchOriginalScript();
